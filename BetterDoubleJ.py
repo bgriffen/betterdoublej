be370bcab68f1fed4e7265846f1f27d1786c10cf
Brendan Griffen <brendan.f.griffen@gmail.com>
2021-09-04 18:44:10 +1000

    added docs

--- BetterDoubleJ.py
+++ BetterDoubleJ.py
@@ -1,3 +1,8 @@
+"""BetterDoublej
+
+This program creates a streaming instance (JJ) and every hour switches to Triple J for the news. It also switches to the news between 6am and 9am when it occurs in 30 minute intervals.
+
+"""
 import sys,os
 
 try:
@@ -9,16 +14,26 @@ import vlc
 import time
 import datetime as dt 
 
-def is_now_between_time_periods(startTime, endTime, nowTime): 
+def is_now_between_time_periods(startTime, endTime, nowTime):
+    """Check if time is between two times.
+    
+    Args:
+        startTime (datetime): start of time to check
+        endTime (datetime): end time to check
+        nowTime (datetime): current time
+    
+    Returns:
+        bool: True/False
+    """
     if startTime < endTime: 
         return nowTime >= startTime and nowTime <= endTime 
     else: 
-        #Over midnight: 
+        # Over midnight 
         return nowTime >= startTime or nowTime <= endTime 
 
 
 class BetterDoubleJ():
-    
+
     def __init__(self):
         #define VLC instance
         self.instance = vlc.Instance('--input-repeat=-1', '--fullscreen')
@@ -28,8 +43,9 @@ class BetterDoubleJ():
         self.playing = ""
 
     def set_station(self,station):
+
+        # Return if already playing selected station
         if station == self.playing:
-            # already playing this station...
             return
 
         if station == "JJ":
@@ -37,45 +53,47 @@ class BetterDoubleJ():
         elif station == "JJJ":
             self.url = 'http://live-radio01.mediahubaustralia.com/4TJW/mp3/'
 
-        #Define VLC media
+        # Define VLC media
         self.media=self.instance.media_new(self.url)
     
-        #Set player media
+        # Set player media
         self.player.set_media(self.media)
         
-        #Play the media
+        # Play the media
         self.player.play()
         
+        # Set playing station
         self.playing = station
 
-    def stop(self):
-        self.player.stop()
-
-    def play(self):
-        self.player.play()
-
     def switcher(self):
+
+        # Current time and hour
         current_time = dt.datetime.now()
         current_hour = int(current_time.hour)
 
-        # true if between 59th minute and 3rd minute after the hour
-        mhour = is_now_between_time_periods(dt.time(current_hour,59,50), dt.time(current_hour,3,30), current_time.time())
+        # Check if time is between 59th minute and 3rd minute after the hour
+        mhour = is_now_between_time_periods(dt.time(current_hour,59,50), 
+                                            dt.time(current_hour,3,30), 
+                                            current_time.time())
         
-        # between 6 and 9am
+        # Is between 6 and 9am create a half hour window
         if current_hour >= 6 and current_hour <= 9:
-            mhalf = is_now_between_time_periods(dt.time(current_hour,30,50), dt.time(current_hour,33,30), current_time.time())
+            mhalf = is_now_between_time_periods(dt.time(current_hour,30,50), 
+                                                dt.time(current_hour,33,30), 
+                                                current_time.time())
             mwindow = mhalf or mhour
         else:
-            # outside of 6 and 9am
+
             mwindow = mhour
 
+        # If satisfied then switch to Triple J (JJJ) 
         if mwindow:
             self.set_station("JJJ")
         else:
             self.set_station("JJ")
 
 if __name__ == '__main__':
-    j = DouglasJ()
+    j = BetterDoubleJ()
     j.set_station("JJ")
     time.sleep(1)
     while True:
