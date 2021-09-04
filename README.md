b208d6d07d5f05f3d2a9df6e488652d223d53d4d
Brendan Griffen <brendan.f.griffen@gmail.com>
2021-09-04 16:59:50 +1000

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Simple streaming app for running Double J but switching to the news on Triple J.
 
+## Requirements
+
+You will need VLC Player and if you want to create a windows executable, you need to run `windowsexe.sh` and have pyinstaller installed.
---
+++ makeexe.sh
@@ -0,0 +1,4 @@
+pyinstaller --noconfirm --onedir --window  --add-binary="snake.ico" "BetterDoubleJ.py"
+
+#--add-data "./axvlc.dll;." --add-data "./libvlc.dll;." --add-data "./libvlccore.dll;." --add-data "./npvlc.dll;."
+# --add-binary="C:/Program Files/VideoLAN/VLC/plugins:plugins"
